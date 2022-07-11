601b7ab4fd8f740c34f0dcc3bc6cd760c0d41ade
Glonee <glonee@foxmail.com>
2022-07-11 15:14:05 +0800

    add close method for syncedBroadcaster

--- syncedBroadcast.go
+++ syncedBroadcast.go
@@ -5,21 +5,24 @@ import "sync"
 //Synced broadcaster provide broadcast without running a goroutine in the background.
 //It should act the same as UnblockedbroadCaster.
 type SyncedBroadcaster[T any] struct {
-	m sync.Map
+	sync.Map
 }
 
 func (b *SyncedBroadcaster[T]) Register(ch chan<- T) {
-	b.m.Store(ch, struct{}{})
+	b.Store(ch, struct{}{})
 }
 func (b *SyncedBroadcaster[T]) Unregister(ch chan<- T) {
-	b.m.Delete(ch)
+	b.Delete(ch)
 }
 func (b *SyncedBroadcaster[T]) Subbmit(m T) {
-	b.m.Range(func(key, _ any) bool {
+	b.Range(func(key, _ any) bool {
 		select {
 		case key.(chan<- T) <- m:
 		default:
 		}
 		return true
 	})
+}
+func (b *SyncedBroadcaster[T]) Close() {
+	*b = SyncedBroadcaster[T]{}
 }
