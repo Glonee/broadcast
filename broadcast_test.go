a55a86fd328d35d7d5177c59c37d8c456877ac0a
Glonee <glonee@foxmail.com>
2022-08-26 09:46:13 +0800

    update to go1.19; update test to cover 100% of statements

--- broadcast_test.go
+++ broadcast_test.go
@@ -1,12 +1,16 @@
 package broadcast
 
 import (
+	"context"
+	"errors"
 	"sync"
 	"testing"
+	"time"
 )
 
 func Test(t *testing.T) {
 	b := NewbroadCaster[int](100)
+	defer b.Close()
 	wg := sync.WaitGroup{}
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
@@ -19,10 +23,27 @@ func Test(t *testing.T) {
 		}()
 	}
 	b.Subbmit(1)
-	wg.Wait()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	finished := make(chan struct{})
+	quit := make(chan struct{})
+	go func() {
+		select {
+		case <-ctx.Done():
+			t.Error(errors.New("not all subscribers received messages"))
+		case <-finished:
+		}
+		quit <- struct{}{}
+	}()
+	go func() {
+		wg.Wait()
+		finished <- struct{}{}
+	}()
+	<-quit
 }
 func Test_unblockedBroadcaster(t *testing.T) {
 	b := NewUnblockedbroadCaster[int](100)
+	defer b.Close()
 	wg := sync.WaitGroup{}
 	for i := 0; i < 10; i++ {
 		cch := make(chan int)
@@ -39,10 +60,27 @@ func Test_unblockedBroadcaster(t *testing.T) {
 		}()
 	}
 	b.Subbmit(1)
-	wg.Wait()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	finished := make(chan struct{})
+	quit := make(chan struct{})
+	go func() {
+		select {
+		case <-ctx.Done():
+			t.Error(errors.New("not all subscribers received messages"))
+		case <-finished:
+		}
+		quit <- struct{}{}
+	}()
+	go func() {
+		wg.Wait()
+		finished <- struct{}{}
+	}()
+	<-quit
 }
 func Test_SyncedBroadCaster(t *testing.T) {
 	b := SyncedBroadcaster[int]{}
+	defer b.Close()
 	wg := sync.WaitGroup{}
 	for i := 0; i < 10; i++ {
 		cch := make(chan int)
@@ -59,5 +97,21 @@ func Test_SyncedBroadCaster(t *testing.T) {
 		}()
 	}
 	b.Subbmit(1)
-	wg.Wait()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	finished := make(chan struct{})
+	quit := make(chan struct{})
+	go func() {
+		select {
+		case <-ctx.Done():
+			t.Error(errors.New("not all subscribers received messages"))
+		case <-finished:
+		}
+		quit <- struct{}{}
+	}()
+	go func() {
+		wg.Wait()
+		finished <- struct{}{}
+	}()
+	<-quit
 }
